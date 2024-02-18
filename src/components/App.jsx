f483add068f9b7fd80ff618f27a43f849e4fcb77
AlexBinkovskyy <alex_bin@ukr.net>
2024-02-18 22:47:38 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,7 @@
-import { Route, Routes } from 'react-router';
-import { Suspense, lazy } from 'react';
-
-import { Header } from './Header/Header';
-import { Page404 } from '../pages/Page404/Page404';
-import  {MovieReviews} from './MovieReviews/MovieReviews';
-
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
-const MovieDetailsPage = lazy(() =>
-  import('../pages/MovieDetailsPage/MovieDetailsPage')
-);
-const MovieCast = lazy(() => import('./MovieCast/MovieCast'));
-
 export const App = () => {
   return (
     <div>
-      <Header />
-      <Suspense fallback={<div>loading...</div>}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="movies" element={<MoviesPage />} />
-          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<MovieCast />} />
-            <Route path="reviews" element={<MovieReviews />} />
-          </Route>
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-      </Suspense>
+      
     </div>
   );
 };
